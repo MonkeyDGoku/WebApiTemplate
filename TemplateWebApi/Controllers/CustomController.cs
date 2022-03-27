b993553e963439ba39fe65ccc34c836cd7b67277
Anup Mahato <anupmahato033@gmail.com>
2022-03-27 20:23:46 +0530

    Add project files.

---
+++ TemplateWebApi/Controllers/CustomController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TemplateWebApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomController : Controller
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<CustomController> _logger;
+
+        public CustomController(ILogger<CustomController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetMessage")]
+        public IActionResult Get()
+        {
+            var str = "Hi there !!!";
+            return Ok(str);
+        }
+    }
+}
